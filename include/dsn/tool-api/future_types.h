4c69f94ecb146dd5852b8ea58dfefe932c40c86f
WeijieSun <sunweijie@xiaomi.com>
2018-05-17 15:06:44 +0800

    task: fix based on comments

--- include/dsn/tool-api/future_types.h
+++ include/dsn/tool-api/future_types.h
@@ -30,7 +30,6 @@
 #include <dsn/tool-api/task.h>
 
 namespace dsn {
-
 typedef std::function<void(dsn::error_code)> err_callback;
 typedef future_task<dsn::error_code> error_code_future;
 typedef dsn::ref_ptr<error_code_future> error_code_future_ptr;
