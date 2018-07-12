75a932d39c2d49bfaf329ebac89b6c7eb7b8659c
WeijieSun <sunweijie@xiaomi.com>
2018-07-12 11:37:01 +0800

    perf-counter: bug fix & refactor
    1. change the value type of perf counters from unsigned to signed
    2. move the feature take_snapshot/query_snapshot/query_counter_by_regexp
       from pegasus to rdsn, so as to support other executables to use these
       features conviniently
    3. remove the useless query_counter interfaces in perf_counters
    4. add some unit tests

--- src/core/tests/perf_counter.cpp
+++ src/core/tests/perf_counter.cpp
@@ -83,7 +83,7 @@ static void perf_counter_add(perf_counter_ptr pc, const std::vector<int> &vec)
         add_threads[i]->join();
 }
 
-TEST(counter, perf_counter_atomic)
+TEST(perf_counter, perf_counter_atomic)
 {
     int ans = 0;
     std::vector<int> vec(10000, 0);
@@ -123,4 +123,42 @@ TEST(counter, perf_counter_atomic)
         for (int i = 0; i != COUNTER_PERCENTILE_COUNT; ++i)
             ddebug("%lf", counter->get_percentile((dsn_perf_counter_percentile_type_t)i));
     }
+}
+
+TEST(perf_counter, print_type)
+{
+    ASSERT_STREQ("NUMBER", counter_type_to_string(COUNTER_TYPE_NUMBER));
+    ASSERT_STREQ("VOLATILE_NUMBER", counter_type_to_string(COUNTER_TYPE_VOLATILE_NUMBER));
+    ASSERT_STREQ("RATE", counter_type_to_string(COUNTER_TYPE_RATE));
+    ASSERT_STREQ("PERCENTILE", counter_type_to_string(COUNTER_TYPE_NUMBER_PERCENTILES));
+    ASSERT_STREQ("INVALID_COUNTER", counter_type_to_string(COUNTER_TYPE_INVALID));
+
+    ASSERT_EQ(COUNTER_TYPE_NUMBER,
+              counter_type_from_string(counter_type_to_string(COUNTER_TYPE_NUMBER)));
+    ASSERT_EQ(COUNTER_TYPE_VOLATILE_NUMBER,
+              counter_type_from_string(counter_type_to_string(COUNTER_TYPE_VOLATILE_NUMBER)));
+    ASSERT_EQ(COUNTER_TYPE_RATE,
+              counter_type_from_string(counter_type_to_string(COUNTER_TYPE_RATE)));
+    ASSERT_EQ(COUNTER_TYPE_NUMBER_PERCENTILES,
+              counter_type_from_string(counter_type_to_string(COUNTER_TYPE_NUMBER_PERCENTILES)));
+    ASSERT_EQ(COUNTER_TYPE_INVALID, counter_type_from_string("xxxx"));
+
+    ASSERT_STREQ("P50", percentile_type_to_string(COUNTER_PERCENTILE_50));
+    ASSERT_STREQ("P90", percentile_type_to_string(COUNTER_PERCENTILE_90));
+    ASSERT_STREQ("P95", percentile_type_to_string(COUNTER_PERCENTILE_95));
+    ASSERT_STREQ("P99", percentile_type_to_string(COUNTER_PERCENTILE_99));
+    ASSERT_STREQ("P999", percentile_type_to_string(COUNTER_PERCENTILE_999));
+    ASSERT_STREQ("INVALID_PERCENTILE", percentile_type_to_string(COUNTER_PERCENTILE_INVALID));
+
+    ASSERT_EQ(COUNTER_PERCENTILE_50,
+              percentile_type_from_string(percentile_type_to_string(COUNTER_PERCENTILE_50)));
+    ASSERT_EQ(COUNTER_PERCENTILE_90,
+              percentile_type_from_string(percentile_type_to_string(COUNTER_PERCENTILE_90)));
+    ASSERT_EQ(COUNTER_PERCENTILE_95,
+              percentile_type_from_string(percentile_type_to_string(COUNTER_PERCENTILE_95)));
+    ASSERT_EQ(COUNTER_PERCENTILE_99,
+              percentile_type_from_string(percentile_type_to_string(COUNTER_PERCENTILE_99)));
+    ASSERT_EQ(COUNTER_PERCENTILE_999,
+              percentile_type_from_string(percentile_type_to_string(COUNTER_PERCENTILE_999)));
+    ASSERT_EQ(COUNTER_PERCENTILE_INVALID, percentile_type_from_string("afafda"));
 }
