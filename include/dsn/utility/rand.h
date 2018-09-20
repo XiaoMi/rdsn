e205f809c78ad401b3a5211361c6110a762ce65d
neverchanje <wutao1@xiaomi.com>
2018-09-20 12:16:01 +0800

    fix comment

--- include/dsn/utility/rand.h
+++ include/dsn/utility/rand.h
@@ -10,19 +10,22 @@
 namespace dsn {
 namespace rand {
 
-// uint64n returns, as an uint64_t, a non-negative pseudo-random number in [min, max).
+// uint64n returns, as an uint64_t, a non-negative pseudo-random number in [min, max].
 extern uint64_t uint64in(uint64_t min, uint64_t max);
 
-// uint64n returns, as an uint64_t, a non-negative pseudo-random number in [0, n).
-inline uint64_t uint64n(uint64_t n) { return uint64in(0, n - 1); }
+// uint64n returns, as an uint64_t, a non-negative pseudo-random number in [0, n].
+inline uint64_t uint64n(uint64_t n) { return uint64in(0, n); }
 
-// uint32n returns, as an uint32_t, a non-negative pseudo-random number in [0, n).
+// uint32n returns, as an uint32_t, a non-negative pseudo-random number in [0, n].
 inline uint32_t uint32n(uint32_t n) { return static_cast<uint32_t>(uint64n(n)); }
 
-// uint32n returns, as an uint32_t, a non-negative pseudo-random number in [min, max).
-inline uint32_t uint32in(uint32_t min, uint32_t max) { return uint32n(max - min) + min; }
+// uint32n returns, as an uint32_t, a non-negative pseudo-random number in [min, max].
+inline uint32_t uint32in(uint32_t min, uint32_t max)
+{
+    return static_cast<uint32_t>(uint64in(min, max));
+}
 
-// float64 returns, as a double, a pseudo-random number in [0.0,1.0).
+// float64 returns, as a double, a pseudo-random number in [0.0,1.0].
 inline double float64n() { return uint64n(1000000000) / 1000000000.0; }
 
 extern void set_seed_thread_local_rng(uint64_t seed);
