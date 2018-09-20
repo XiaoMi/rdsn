43bd11d2f28526ec70e4b0e20375476e99ec5505
neverchanje <wutao1@xiaomi.com>
2018-09-20 16:10:52 +0800

    minor fixes

--- include/dsn/utility/rand.h
+++ include/dsn/utility/rand.h
@@ -27,13 +27,13 @@ inline uint64_t uint64n(uint64_t n) { return uint64in(0, n - 1); }
 inline uint32_t uint32n(uint32_t n) { return static_cast<uint32_t>(uint64n(n)); }
 
 // uint32 returns a pseudo-random 32-bit value as a uint32_t.
-inline uint32_t uint32() { return uint32n(std::numeric_limits<uint32_t>::max()); }
+inline uint32_t uint32() { return uint32in(0, std::numeric_limits<uint32_t>::max()); }
 
 // uint64 returns a pseudo-random 64-bit value as a uint64_t.
-inline uint64_t uint64() { return uint64n(std::numeric_limits<uint64_t>::max()); }
+inline uint64_t uint64() { return uint64in(0, std::numeric_limits<uint64_t>::max()); }
 
 // float64 returns, as a double, a pseudo-random number in [0.0,1.0].
-inline double float64n() { return uint64n(1000000000) / 1000000000.0; }
+inline double float64n() { return uint64in(0, 1000000000) / 1000000000.0; }
 
 extern void set_seed_thread_local_rng(uint64_t seed);
 
