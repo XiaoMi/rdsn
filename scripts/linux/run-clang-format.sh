7d65f71af27b313b22bad65aba2eab1d080005b8
neverchanje <wutao1@xiaomi.com>
2018-12-21 15:26:49 +0800

    travis: run clang-format before building

---
+++ scripts/linux/run-clang-format.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# How to use this hook?
+# ln -s hooks/pre-commit .git/hooks/
+# In case hook is not executable
+# chmod +x .git/hooks/pre-commit
+
+find src include -name "*.h" -or -name "*.cpp" | xargs clang-format-3.9 -i
+
+effected_files=$(git status -s)
+echo -ne "Checking for files that need clang-format... "
+if [ -z "${effected_files}" ]; then
+	echo "All files are well formatted"
+	exit 0
+else
+	echo "Please check if the following files are well formatted:"
+	echo "${effected_files}"
+	exit 1
+fi
