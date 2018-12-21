8c64a5dd69174b1618af105804cf7773c77ad7bc
neverchanje <wutao1@xiaomi.com>
2018-12-21 15:34:19 +0800

    remove comments

--- scripts/linux/run-clang-format.sh
+++ scripts/linux/run-clang-format.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# How to use this hook?
-# ln -s hooks/pre-commit .git/hooks/
-# In case hook is not executable
-# chmod +x .git/hooks/pre-commit
-
 find src include -name "*.h" -or -name "*.cpp" | xargs clang-format-3.9 -i
 
 effected_files=$(git status -s)
