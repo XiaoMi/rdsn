5275e53c643654f5504c7a0187a0b91d3d4ae8e4
neverchanje <wutao1@xiaomi.com>
2019-04-14 13:00:32 +0800

    fix

--- src/dist/replication/meta_server/meta_service.cpp
+++ src/dist/replication/meta_server/meta_service.cpp
@@ -24,14 +24,6 @@
  * THE SOFTWARE.
  */
 
-/*
- * Description:
- *     What is this file about?
- *
- * Revision history:
- *     xxxx-xx-xx, author, first version
- *     xxxx-xx-xx, author, fix bug about xxx
- */
 #include <sys/stat.h>
 
 #include <boost/lexical_cast.hpp>
@@ -41,6 +33,7 @@
 #include <dsn/utility/extensible_object.h>
 #include <dsn/utility/string_conv.h>
 #include <dsn/dist/meta_state_service.h>
+#include <dsn/dist/replication/duplication_common.h>
 #include <dsn/tool-api/command_manager.h>
 #include <algorithm> // for std::remove_if
 #include <cctype>    // for ::isspace
@@ -49,6 +42,7 @@
 #include "server_state.h"
 #include "meta_server_failure_detector.h"
 #include "server_load_balancer.h"
+#include "dist/replication/meta_server/duplication/meta_duplication_service.h"
 
 namespace dsn {
 namespace replication {
@@ -107,6 +101,7 @@ error_code meta_service::remote_storage_initialize()
         return err;
     }
     _storage.reset(storage);
+    _meta_storage.reset(new mss::meta_storage(_storage.get(), &_tracker));
 
     std::vector<std::string> slices;
     utils::split_args(_meta_opts.cluster_root.c_str(), slices, '/');
@@ -318,6 +313,8 @@ error_code meta_service::start()
                err.to_string());
     }
 
+    initialize_duplication_service();
+
     _state->register_cli_commands();
 
     start_service();
@@ -364,6 +361,7 @@ void meta_service::register_rpc_handlers()
     register_rpc_handler(RPC_CM_QUERY_RESTORE_STATUS,
                          "query_restore_status",
                          &meta_service::on_query_restore_status);
+
     register_rpc_handler_with_rpc_holder(
         RPC_CM_UPDATE_APP_ENV, "update_app_env(set/del/clear)", &meta_service::update_app_env);
     register_rpc_handler_with_rpc_holder(
@@ -786,6 +784,15 @@ void meta_service::on_query_restore_status(dsn::message_ex *req)
                      std::bind(&server_state::on_query_restore_status, _state.get(), req));
 }
 
+void meta_service::initialize_duplication_service()
+{
+    if (!_opts.duplication_disabled) {
+        _dup_svc = dsn::make_unique<meta_duplication_service>(_state.get(), this);
+    } else {
+        _dup_svc.reset(nullptr);
+    }
+}
+
 void meta_service::update_app_env(app_env_rpc env_rpc)
 {
     auto &response = env_rpc.response();
@@ -825,5 +832,6 @@ void meta_service::ddd_diagnose(ddd_diagnose_rpc rpc)
     get_balancer()->get_ddd_partitions(rpc.request().pid, response.partitions);
     response.err = ERR_OK;
 }
-}
-}
+
+} // namespace replication
+} // namespace dsn
