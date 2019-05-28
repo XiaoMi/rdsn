5c4827452f35591fed175441c50fbc42c37acc42
neverchanje <wutao1@xiaomi.com>
2019-05-28 17:42:36 +0800

    revert removal of original string_conv implementation

--- include/dsn/utility/string_conv.h
+++ include/dsn/utility/string_conv.h
@@ -33,15 +33,98 @@
 
 namespace dsn {
 
+namespace internal {
+
+template <typename T>
+bool buf2signed(string_view buf, T &result)
+{
+    static_assert(std::is_signed<T>::value, "buf2signed works only with signed integer");
+
+    if (buf.empty()) {
+        return false;
+    }
+
+    std::string str(buf.data(), buf.length());
+    const int saved_errno = errno;
+    errno = 0;
+    char *p = nullptr;
+    long long v = std::strtoll(str.data(), &p, 0);
+
+    if (p - str.data() != str.length()) {
+        return false;
+    }
+
+    if (v > std::numeric_limits<T>::max() || v < std::numeric_limits<T>::min() || errno != 0) {
+        return false;
+    }
+
+    if (errno == 0) {
+        errno = saved_errno;
+    }
+
+    result = v;
+    return true;
+}
+
+template <typename T>
+bool buf2unsigned(string_view buf, T &result)
+{
+    static_assert(std::is_unsigned<T>::value, "buf2unsigned works only with unsigned integer");
+
+    if (buf.empty()) {
+        return false;
+    }
+
+    std::string str(buf.data(), buf.length());
+    const int saved_errno = errno;
+    errno = 0;
+    char *p = nullptr;
+    unsigned long long v = std::strtoull(str.data(), &p, 0);
+
+    if (p - str.data() != str.length()) {
+        return false;
+    }
+
+    if (v > std::numeric_limits<T>::max() || v < std::numeric_limits<T>::min() || errno != 0) {
+        return false;
+    }
+
+    if (errno == 0) {
+        errno = saved_errno;
+    }
+
+    // strtoull() will convert a negative integer to an unsigned integer,
+    // return false in this condition. (but we consider "-0" is correct)
+    if (v != 0 && str.find('-') != std::string::npos) {
+        return false;
+    }
+
+    result = v;
+    return true;
+}
+} // namespace internal
+
 /// buf2*: `result` will keep unmodified if false is returned.
 
-extern bool buf2int32(string_view buf, int32_t &result);
+inline bool buf2int32(string_view buf, int32_t &result)
+{
+    return internal::buf2signed(buf, result);
+}
 
-extern bool buf2int64(string_view buf, int64_t &result);
+inline bool buf2int64(string_view buf, int64_t &result)
+{
+    return internal::buf2signed(buf, result);
+}
 
-extern bool buf2uint64(string_view buf, uint64_t &result);
+inline bool buf2uint32(string_view buf, uint32_t &result)
+{
+    return internal::buf2unsigned(buf, result);
+}
 
-extern bool buf2uint32(string_view buf, uint32_t &result);
+inline bool buf2uint64(string_view buf, uint64_t &result)
+{
+    return internal::buf2unsigned(buf, result);
+}
 
 inline bool buf2bool(string_view buf, bool &result, bool ignore_case = true)
 {
@@ -60,16 +143,31 @@ inline bool buf2bool(string_view buf, bool &result, bool ignore_case = true)
     return false;
 }
 
-extern bool buf2float(string_view buf, float &result);
+inline bool buf2double(string_view buf, double &result)
+{
+    if (buf.empty()) {
+        return false;
+    }
 
-extern bool buf2double(string_view buf, double &result);
+    std::string str(buf.data(), buf.length());
+    const int saved_errno = errno;
+    errno = 0;
+    char *p = nullptr;
+    double v = std::strtod(str.data(), &p);
 
-inline bool buf2int(string_view buf, uint32_t &result) { return buf2uint32(buf, result); }
+    if (p - str.data() != str.length()) {
+        return false;
+    }
 
-inline bool buf2int(string_view buf, uint64_t &result) { return buf2uint64(buf, result); }
+    if (v == HUGE_VAL || v == -HUGE_VAL || std::isnan(v) || errno != 0) {
+        return false;
+    }
 
-inline bool buf2int(string_view buf, int64_t &result) { return buf2int64(buf, result); }
+    if (errno == 0) {
+        errno = saved_errno;
+    }
 
-inline bool buf2int(string_view buf, int32_t &result) { return buf2int32(buf, result); }
-
+    result = v;
+    return true;
+}
 } // namespace dsn
