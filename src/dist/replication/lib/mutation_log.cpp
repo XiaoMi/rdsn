fbe1dfdcebed7dc5bc75b9f6402125f8a3802dd3
neverchanje <wutao1@xiaomi.com>
2019-09-10 22:40:10 +0800

    minor fix

--- src/dist/replication/lib/mutation_log.cpp
+++ src/dist/replication/lib/mutation_log.cpp
@@ -1143,10 +1143,9 @@ bool mutation_log::get_learn_state(gpid gpid, decree start, /*out*/ learn_state 
 
         if (state.meta.length() == 0 && start > _private_log_info.max_decree) {
             // no memory data and no disk data
-            ddebug_f("gpid({}.{}) get_learn_state returns false"
+            ddebug_f("gpid({}) get_learn_state returns false"
                      "learn_start_decree={}, max_decree_in_private_log={}",
-                     gpid.get_app_id(),
-                     gpid.get_partition_index(),
+                     gpid,
                      start,
                      _private_log_info.max_decree);
             return false;
