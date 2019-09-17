dccfac2e90b2096c2d70b648dd6f6b6aea9e410f
zhaoliwei <zhaoliwei@xiaomi.com>
2019-09-17 10:19:05 +0800

    get_app_env http api

---
+++ src/dist/http/app_env_http_service.h
@@ -0,0 +1,12 @@
+//
+// Created by mi on 2019/9/16.
+//
+
+#ifndef DSN_APP_ENV_HTTP_SERVICE_H
+#define DSN_APP_ENV_HTTP_SERVICE_H
+
+class app_env_http_service
+{
+};
+
+#endif // DSN_APP_ENV_HTTP_SERVICE_H
