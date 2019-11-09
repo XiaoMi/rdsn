b451f8bb30e68358e7372683e6ca3986b3e36e0f
TangYanzhao <527646889@qq.com>
2019-11-09 23:19:39 +0800

    1

--- src/dist/replication/test/replica_test/unit_test/hostname.cpp
+++ src/dist/replication/test/replica_test/unit_test/hostname.cpp
@@ -4,6 +4,7 @@
 
 #include <dsn/dist/replication/replication_ddl_client.h>
 #include <gtest/gtest.h>
+#include <dsn/tool-api/rpc_address.h>
 
 using namespace ::dsn;
 using namespace ::dsn::replication;
@@ -14,13 +15,32 @@ static bool hostname_from_ip(uint32_t ip, std::string* ip_address);
 static bool hostname_from_ip(const char *ip,std::string *ip_address);
 static bool hostname_from_ip_port(uint32_t ip, std::string *ip_address);
 static bool hostname_from_ip_port(const char *ip_port,std::string *ip_address);
-static bool list_hostname_from_ip(const char *ip_port_list,std::string *ip_address_list);     static bool list_hostname_from_ip_port(const char *ip_port_list,std::string *ip_address_list);
+static bool list_hostname_from_ip(const char *ip_port_list,std::string *ip_address_list);
+static bool list_hostname_from_ip_port(const char *ip_port_list,std::string *ip_address_list);
 */
 
-TEST(ip_to_hostname, locolhost) {
-    std::string ip1 = "127.0.0.1";
+TEST(ip_to_hostname, locolhost)
+{
+    rpc_address rpc_example;
+    rpc_example.assign_ipv4("127.0.0.1", 23010);
     std::string hostnameResult;
-    replication_ddl_client::hostname_from_ip_port(ip1.c_str(), &hostnameResult);
-    EXPECT_EQ(0, replication_ddl_client::hostname_from_ip_port(ip1.c_str(), &hostnameResult));
-    EXPECT_EQ(1, replication_ddl_client::hostname_from_ip_port("10.232.55.200", &hostnameResult));
+    // static bool hostname(const rpc_address &address,std::string *ip_address);
+    EXPECT_EQ(0, replication_ddl_client::hostname(rpc_example, &hostnameResult));
+
+    // static bool hostname_from_ip(uint32_t ip, std::string* ip_address);
+    uint32_t ip_example = 2130706433; // 127.0.0.1
+    EXPECT_EQ(0, replication_ddl_client::hostname_from_ip(ip_example, &hostnameResult));
+
+    // static bool hostname_from_ip(const char *ip,std::string *ip_address);
+    EXPECT_EQ(0, replication_ddl_client::hostname_from_ip("127.0.0.1", &hostnameResult));
+
+    //static bool hostname_from_ip_port(const char *ip_port,std::string *ip_address);
+    EXPECT_EQ(0, replication_ddl_client::hostname_from_ip_port("127.0.0.1:8080", &hostnameResult));
+
+    //static bool list_hostname_from_ip(const char *ip_port_list,std::string *ip_address_list);
+    EXPECT_EQ(0, replication_ddl_client::list_hostname_from_ip("127.0.0.1,127.0.0.2,127.0.0.3", &hostnameResult));
+
+    //static bool list_hostname_from_ip_port(const char *ip_port_list,std::string *ip_address_list);
+    EXPECT_EQ(0, replication_ddl_client::list_hostname_from_ip("127.0.0.1:23456,127.0.0.2:22233,127.0.0.3:12345", &hostnameResult));
+
 }
