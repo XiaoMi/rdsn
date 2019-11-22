fe36db730f6dbcb7f726390284b03e3de607414b
Tang Yanzhao <527646889@qq.com>
2019-11-22 18:49:08 +0800

    1

--- src/dist/replication/meta_server/meta_http_service.cpp
+++ src/dist/replication/meta_server/meta_http_service.cpp
@@ -10,7 +10,6 @@
 #include <dsn/dist/replication/replication_types.h>
 #include <dsn/utility/config_api.h>
 #include <dsn/utility/output_utils.h>
-#include <dsn/tool-api/task.h>
 
 #include "server_load_balancer.h"
 #include "server_state.h"
