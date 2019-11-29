e0a14906861bac809cdb3d5f1ab30b51f35e8aa0
Tang Yanzhao <527646889@qq.com>
2019-11-29 11:45:20 +0800

    change 1

---
+++ back1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -z "${REPORT_DIR}" ]; then
+    REPORT_DIR="."
+fi
+
+./clear.sh
+output_xml="${REPORT_DIR}/dsn.meta.test.3.xml"
+GTEST_OUTPUT="xml:${output_xml}" GTEST_FILTER="meta_http_service_test.*" ./dsn.meta.test
+if [ $? -ne 0 ]; then
+    tail -n 100 data/log/log.1.txt
+    if [ -f core ]; then
+        echo "---- gdb ./dsn.meta.test core ----"
+        gdb ./dsn.meta.test core -ex "thread apply all bt" -ex "set pagination 0" -batch
+    fi
+    exit 1
+fi
