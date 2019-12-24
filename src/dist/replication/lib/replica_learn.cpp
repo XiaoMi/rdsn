212d54ed0feed074a2485d97531b320269f4f577
neverchanje <wutao1@xiaomi.com>
2019-12-24 20:03:07 +0800

    fix compile

--- src/dist/replication/lib/replica_learn.cpp
+++ src/dist/replication/lib/replica_learn.cpp
@@ -1531,7 +1531,7 @@ error_code replica::apply_learned_state_from_private_log(learn_state &state)
     prepare_list plist(this,
                        _app->last_committed_decree(),
                        _options->max_mutation_count_in_prepare_list,
-                       [this](mutation_ptr &mu) {
+                       [this, duplicating](mutation_ptr &mu) {
                            if (mu->data.header.decree == _app->last_committed_decree() + 1) {
                                // TODO: assign the returned error_code to err and check it
                                _app->apply_mutation(mu);
