af006c788c81c0445890a94cdb5f88ef2154a982
zhaoliwei <zhaoliwei@xiaomi.com>
2020-02-25 10:44:25 +0800

    refactor

--- src/core/core/logger.cpp
+++ src/core/core/logger.cpp
@@ -24,16 +24,6 @@
  * THE SOFTWARE.
  */
 
-/*
- * Description:
- * Description:
- *     What is this file about?
- *
- * Revision history:
- *     xxxx-xx-xx, author, first version
- *     xxxx-xx-xx, author, fix bug about xxx
- */
-
 #include <dsn/utility/logging_provider.h>
 #include <dsn/tool_api.h>
 #include <dsn/utility/time_utils.h>
