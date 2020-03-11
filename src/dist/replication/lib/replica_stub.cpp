47d95b48a36d6304edf5cdabea1104b7b8317888
JiaShuo <js982986555@live.com>
2020-03-11 14:20:26 +0800

    add test and counter

--- src/dist/replication/lib/replica_stub.cpp
+++ src/dist/replication/lib/replica_stub.cpp
@@ -87,10 +87,9 @@ replica_stub::replica_stub(replica_state_subscriber subscriber /*= nullptr*/,
     install_perf_counters();
 
     _max_allowed_write_size = dsn_config_get_value_uint64(
-        "pegasus.server",
-        "max_allowed_write_size",
-        1 << 20,
-        "write operation exceed this threshold will be logged and reject, 0 means no check");
+        "pegasus.server", "max_allowed_write_size", 1048576, "write operation exceed this "
+                                                             "threshold will be logged and reject, "
+                                                             "default is 1MB, 0 means no check");
 }
 
 replica_stub::~replica_stub(void) { close(); }
