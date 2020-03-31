a15d8d73246f3dc6360389d6dfd5b20b3b7ad886
zhaoliwei <zhaoliwei@xiaomi.com>
2020-03-31 18:16:37 +0800

    fix

--- src/dist/replication/lib/backup/replica_backup_manager.cpp
+++ src/dist/replication/lib/backup/replica_backup_manager.cpp
@@ -44,7 +44,8 @@ static bool get_policy_checkpoint_dirs(const std::string &dir,
     return true;
 }
 
-replica_backup_manager::replica_backup_manager(replica *r) : replica_base(r), _replica(r) {
+replica_backup_manager::replica_backup_manager(replica *r) : replica_base(r), _replica(r)
+{
     _collect_info_timer =
         tasking::enqueue_timer(LPC_PER_REPLICA_COLLECT_INFO_TIMER,
                                &_replica->_tracker,
@@ -53,9 +54,7 @@ replica_backup_manager::replica_backup_manager(replica *r) : replica_base(r), _r
                                get_gpid().thread_hash());
 }
 
-replica_backup_manager::~replica_backup_manager() {
-    _collect_info_timer->cancel(false);
-}
+replica_backup_manager::~replica_backup_manager() { _collect_info_timer->cancel(false); }
 
 void replica_backup_manager::on_cold_backup_clear(const backup_clear_request &request)
 {
@@ -89,30 +88,30 @@ void replica_backup_manager::collect_backup_info()
     uint64_t cold_backup_max_duration_time_ms = 0;
     uint64_t cold_backup_max_upload_file_size = 0;
     uint64_t now_ms = dsn_now_ms();
+
+    // collect backup info from all of the cold backup contexts
     for (const auto &p : _replica->_cold_backup_contexts) {
         const cold_backup_context_ptr &backup_context = p.second;
         cold_backup_status backup_status = backup_context->status();
         if (_replica->status() == partition_status::type::PS_PRIMARY) {
-            if (backup_status != ColdBackupInvalid && backup_status != ColdBackupCompleted &&
-                backup_status != ColdBackupCanceled && backup_status != ColdBackupFailed) {
+            if (backup_status > ColdBackupInvalid && backup_status < ColdBackupCanceled) {
                 cold_backup_running_count++;
             }
         } else if (_replica->status() == partition_status::type::PS_SECONDARY) {
-            if (backup_status != ColdBackupInvalid && backup_status != ColdBackupFailed &&
-                backup_status != ColdBackupCanceled && backup_status != ColdBackupCheckpointed) {
-                // secondary end backup with status ColdBackupCheckpointed
+            // secondary end backup with status ColdBackupCheckpointed
+            if (backup_status > ColdBackupInvalid && backup_status < ColdBackupCheckpointed) {
                 cold_backup_running_count++;
             }
         }
 
         if (backup_status == ColdBackupUploading) {
-            cold_backup_max_duration_time_ms =
-                std::max(cold_backup_max_duration_time_ms,
-                         (now_ms - backup_context->get_start_time_ms()));
+            cold_backup_max_duration_time_ms = std::max(
+                cold_backup_max_duration_time_ms, now_ms - backup_context->get_start_time_ms());
             cold_backup_max_upload_file_size =
                 std::max(cold_backup_max_upload_file_size, backup_context->get_upload_file_size());
         }
     }
+
     _replica->_cold_backup_running_count.store(cold_backup_running_count);
     _replica->_cold_backup_max_duration_time_ms.store(cold_backup_max_duration_time_ms);
     _replica->_cold_backup_max_upload_file_size.store(cold_backup_max_upload_file_size);
