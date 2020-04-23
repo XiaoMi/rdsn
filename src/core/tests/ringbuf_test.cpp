fa353c831e09699841b37a0887a8a7451fef37ac
smityz <smityz@qq.com>
2020-04-23 15:00:16 +0800

    add unit test

--- src/core/tests/ringbuf_test.cpp
+++ src/core/tests/ringbuf_test.cpp
@@ -1,24 +1,9 @@
+// Copyright (c) 2017, Xiaomi, Inc.  All rights reserved.
+// This source code is licensed under the Apache License Version 2.0, which
+// can be found in the LICENSE file in the root directory of this source tree.
+
 #include <dsn/utility/ringbuf.h>
 #include <gtest/gtest.h>
-
-/*
-
-Result:
-1 1 1
-1 2 2
-1 3 3
-1 4 4
-1 5 5
-2 6 5
-3 7 5
--------
-3 7 5
-4 7 4
-5 7 3
-6 7 2
-7 7 1
-
-*/
 
 TEST(ringbuf_test, function_test)
 {
