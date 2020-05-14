4f8d2a0f610cd2d083d8134098c03fa576fc2fdc
smityz <smityz@qq.com>
2020-05-14 16:31:51 +0800

    add lightweightsemaphore.h

--- thirdparty/build-thirdparty.sh
+++ thirdparty/build-thirdparty.sh
@@ -52,7 +52,7 @@ mkdir -p $TP_OUTPUT/bin
 if [ ! -d $TP_OUTPUT/include/concurrentqueue ]; then
     cd $TP_SRC/concurrentqueue-1.0.1
     mkdir -p $TP_OUTPUT/include/concurrentqueue
-    cp -R blockingconcurrentqueue.h concurrentqueue.h internal/ $TP_OUTPUT/include/concurrentqueue
+    cp -R blockingconcurrentqueue.h concurrentqueue.h lightweightsemaphore.h internal/ $TP_OUTPUT/include/concurrentqueue
     cd $TP_DIR
     exit_if_fail "concurrentqueue" $?
 else
