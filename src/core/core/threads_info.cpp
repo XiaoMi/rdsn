8f33625672b7f19c2f7e66a8b633aa9d06fcdf29
Smilencer <527646889@qq.com>
2020-05-19 11:09:40 +0800

    Update threads_info.cpp
    License

--- src/core/core/threads_info.cpp
+++ src/core/core/threads_info.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2017-present, Xiaomi, Inc.  All rights reserved.
+// This source code is licensed under the Apache License Version 2.0, which
+// can be found in the LICENSE file in the root directory of this source tree.
+
 #include <dsn/tool-api/threads_info.h>
 
 #include "service_engine.h"
