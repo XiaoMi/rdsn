f55c87040d04d2dd08ab27b4e224c57520f409b0
JiaShuo <js982986555@live.com>
2020-05-19 18:05:56 +0800

    init not compile

--- include/dsn/tool/lantency_tracer.h
+++ include/dsn/tool/lantency_tracer.h
@@ -12,13 +12,13 @@ public:
     lantency_tracer(int id, const std::string &name)
         : _id(id), _start_time(dsn_now_ms()), _last_time(_start_time){};
 
-    void add_point(const std::string &name, long current_time)
+    void add_point(const std::string &name, uint64_t current_time)
     {
         _points.emplace(name, current_time - _last_time);
         _last_time = current_time;
     }
 
-    void add_point(const std::string &name, long current_time, long last_time)
+    void add_point(const std::string &name, uint64_t current_time, uint64_t last_time)
     {
         _points.emplace(name, current_time - last_time);
         _last_time = current_time;
@@ -26,27 +26,27 @@ public:
 
     void set_type(std::string type) { _type = type; };
 
-    void set_end_time(long end_time) { _end_time = end_time; }
+    void set_end_time(uint64_t end_time) { _end_time = end_time; }
 
-    long get_last_time() { return _last_time; }
+    uint64_t get_last_time() { return _last_time; }
 
-    long get_start_time() { return _start_time; }
+    uint64_t get_start_time() { return _start_time; }
 
-    long get_end_time() { return _end_time; }
+    uint64_t get_end_time() { return _end_time; }
 
-    std::map<std::string, long> &get_points() { return _points; }
+    std::map<std::string, uint64_t> &get_points() { return _points; }
 
     //(todo jiashuo1) async report to server (db(maybe pegasus is ok) or falcon or kibana)
     void report(){};
 
 private:
     int _id;
-    long _start_time;
-    long _last_time;
-    long _end_time;
+    uint64_t _start_time;
+    uint64_t _last_time;
+    uint64_t _end_time;
     std::string _name;
     std::string _type;
-    std::map<std::string, long> _points;
+    std::map<std::string, uint64_t> _points;
 };
 }
 }
