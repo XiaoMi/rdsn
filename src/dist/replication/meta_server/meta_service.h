f1298ea19df4db33bd434dfbc3c22f409a370a9e
zhaoliwei <zhaoliwei@xiaomi.com>
2020-06-10 19:00:43 +0800

    fix

--- src/dist/replication/meta_server/meta_service.h
+++ src/dist/replication/meta_server/meta_service.h
@@ -217,11 +217,11 @@ private:
     //   0. meta isn't leader, and rpc-msg can forward to others
     //  -1. meta isn't leader, and rpc-msg can't forward to others
     // if return -1 and `forward_address' != nullptr, then return leader by `forward_address'.
+    int check_leader(dsn::message_ex *req, dsn::rpc_address *forward_address);
     template <typename TRpcHolder>
     int check_leader(TRpcHolder rpc, /*out*/ rpc_address *forward_address);
     template <typename TRpcHolder>
     bool check_status(TRpcHolder rpc, /*out*/ rpc_address *forward_address = nullptr);
-    int check_leader(dsn::message_ex *req, dsn::rpc_address *forward_address);
     error_code remote_storage_initialize();
     bool check_freeze() const;
 
