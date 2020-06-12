3cf694be8c07eff5fa86fe092e123bec167c1b01
Shuo <js982986555@live.com>
2020-06-12 11:50:00 +0800

    Update src/dist/replication/meta_server/greedy_load_balancer.cpp
    Co-authored-by: Yingchun Lai <405403881@qq.com>

--- src/dist/replication/meta_server/greedy_load_balancer.cpp
+++ src/dist/replication/meta_server/greedy_load_balancer.cpp
@@ -834,7 +834,7 @@ void greedy_load_balancer::greedy_balancer(const bool balance_checker)
     for (const auto &kv : apps) {
         const std::shared_ptr<app_state> &app = kv.second;
         if (in_ignored_apps(kv.first)) {
-            ddebug_f("stop to do primary balance for the ignored app[{}]", app->get_logname());
+            ddebug_f("skip to do primary balance for the ignored app[{}]", app->get_logname());
             continue;
         }
         if (app->status != app_status::AS_AVAILABLE)
