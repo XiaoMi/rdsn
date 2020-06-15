fe4d134eb591bb64d7a61a6b192db1b49bdf2f29
heyuchen <heyuchen@xiaomi.com>
2020-06-15 14:19:29 +0800

    meta handle bulk load response during ingestion

--- src/dist/replication/meta_server/meta_bulk_load_service.cpp
+++ src/dist/replication/meta_server/meta_bulk_load_service.cpp
@@ -514,8 +514,58 @@ void bulk_load_service::handle_app_downloading(const bulk_load_response &respons
 void bulk_load_service::handle_app_ingestion(const bulk_load_response &response,
                                              const rpc_address &primary_addr)
 {
-    // TODO(heyuchen): TBD
-    // called by `on_partition_bulk_load_reply` when app status is ingesting
+    const std::string &app_name = response.app_name;
+    const gpid &pid = response.pid;
+
+    if (!response.__isset.is_group_ingestion_finished) {
+        dwarn_f("recevie bulk load response from node({}) app({}) partition({}), "
+                "primary_status({}), but is_group_ingestion_finished is not set",
+                primary_addr.to_string(),
+                response.app_name,
+                pid,
+                dsn::enum_to_string(response.primary_bulk_load_status));
+        return;
+    }
+
+    for (const auto &kv : response.group_bulk_load_state) {
+        const auto &bulk_load_states = kv.second;
+        if (!bulk_load_states.__isset.ingest_status) {
+            dwarn_f("recevie bulk load response from node({}) app({}) partition({}), "
+                    "primary_status({}), but node({}) ingestion_status is not set",
+                    primary_addr.to_string(),
+                    response.app_name,
+                    pid,
+                    dsn::enum_to_string(response.primary_bulk_load_status),
+                    kv.first.to_string());
+            return;
+        }
+
+        if (bulk_load_states.ingest_status == ingestion_status::IS_FAILED) {
+            derror_f("app({}) partition({}) node({}) ingestion failed",
+                     app_name,
+                     pid,
+                     kv.first.to_string());
+            handle_bulk_load_failed(pid.get_app_id());
+            return;
+        }
+    }
+
+    ddebug_f("recevie bulk load response from node({}) app({}) partition({}), primary_status({}), "
+             "is_group_ingestion_finished = {}",
+             primary_addr.to_string(),
+             app_name,
+             pid,
+             dsn::enum_to_string(response.primary_bulk_load_status),
+             response.is_group_ingestion_finished);
+    {
+        zauto_write_lock l(_lock);
+        _partitions_bulk_load_state[pid] = response.group_bulk_load_state;
+    }
+
+    if (response.is_group_ingestion_finished) {
+        ddebug_f("app({}) partition({}) ingestion files succeed", app_name, pid);
+        update_partition_status_on_remote_storage(app_name, pid, bulk_load_status::BLS_SUCCEED);
+    }
 }
 
 // ThreadPool: THREAD_POOL_META_STATE
@@ -635,6 +685,7 @@ void bulk_load_service::update_partition_status_on_remote_storage_reply(
         switch (new_status) {
         case bulk_load_status::BLS_DOWNLOADED:
         case bulk_load_status::BLS_INGESTING:
+        case bulk_load_status::BLS_SUCCEED:
             if (old_status != new_status && --_apps_in_progress_count[pid.get_app_id()] == 0) {
                 update_app_status_on_remote_storage_unlocked(pid.get_app_id(), new_status);
             }
