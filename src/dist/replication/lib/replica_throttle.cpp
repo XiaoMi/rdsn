998fc4af7be11993f1c278f0ad3908bc8fdadfc7
levy <zlw5307@163.com>
2020-07-09 15:43:41 +0800

    throttle-controller

--- src/dist/replication/lib/replica_throttle.cpp
+++ src/dist/replication/lib/replica_throttle.cpp
@@ -23,24 +23,23 @@ bool replica::throttle_request(throttling_controller &controller,
     }
 
     int64_t delay_ms = 0;
-    auto type = controller.control(request, request_units, delay_ms);
+    auto type = controller.control(request->header->client.timeout_ms, request_units, delay_ms);
     if (type != throttling_controller::PASS) {
         if (type == throttling_controller::DELAY) {
             tasking::enqueue(LPC_WRITE_THROTTLING_DELAY,
                              &_tracker,
-                             [ this, req = message_ptr(request) ]() { on_client_write(req, true); },
+                             [this, req = message_ptr(request)]() { on_client_write(req, true); },
                              get_gpid().thread_hash(),
                              std::chrono::milliseconds(delay_ms));
             _counter_recent_write_throttling_delay_count->increment();
         } else { // type == throttling_controller::REJECT
             if (delay_ms > 0) {
-                tasking::enqueue(LPC_WRITE_THROTTLING_DELAY,
-                                 &_tracker,
-                                 [ this, req = message_ptr(request) ]() {
-                                     response_client_write(req, ERR_BUSY);
-                                 },
-                                 get_gpid().thread_hash(),
-                                 std::chrono::milliseconds(delay_ms));
+                tasking::enqueue(
+                    LPC_WRITE_THROTTLING_DELAY,
+                    &_tracker,
+                    [this, req = message_ptr(request)]() { response_client_write(req, ERR_BUSY); },
+                    get_gpid().thread_hash(),
+                    std::chrono::milliseconds(delay_ms));
             } else {
                 response_client_write(request, ERR_BUSY);
             }
