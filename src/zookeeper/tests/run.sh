9de18cfc78da9d6c15aba963940a6fe585da3eef
levy <zlw5307@163.com>
2020-07-09 11:03:56 +0800

    refactor

--- src/zookeeper/tests/run.sh
+++ src/zookeeper/tests/run.sh
@@ -7,13 +7,13 @@ fi
 while read -r -a line; do
     test_case=${line[0]}
     gtest_filter=${line[1]}
-    output_xml="${REPORT_DIR}/dsn.tests_${test_case/.ini/.xml}"
-    echo "============ run dsn.tests ${test_case} with gtest_filter ${gtest_filter} ============"
+    output_xml="${REPORT_DIR}/dsn.zookeeper.tests_${test_case/.ini/.xml}"
+    echo "============ run dsn.zookeeper.tests ${test_case} with gtest_filter ${gtest_filter} ============"
     ./clear.sh
-    GTEST_OUTPUT="xml:${output_xml}" GTEST_FILTER=${gtest_filter} ./dsn.tests ${test_case}
+    GTEST_OUTPUT="xml:${output_xml}" GTEST_FILTER=${gtest_filter} ./dsn.zookeeper.tests ${test_case}
 
     if [ $? -ne 0 ]; then
-        echo "run dsn.tests $test_case failed"
+        echo "run dsn.zookeeper.tests $test_case failed"
         echo "---- ls ----"
         ls -l
         if find . -name log.1.txt; then
@@ -21,10 +21,10 @@ while read -r -a line; do
             tail -n 100 `find . -name log.1.txt`
         fi
         if [ -f core ]; then
-            echo "---- gdb ./dsn.tests core ----"
-            gdb ./dsn.tests core -ex "thread apply all bt" -ex "set pagination 0" -batch
+            echo "---- gdb ./dsn.zookeeper.tests core ----"
+            gdb ./dsn.zookeeper.tests core -ex "thread apply all bt" -ex "set pagination 0" -batch
         fi
         exit 1
     fi
-    echo "============ done dsn.tests ${test_case} with gtest_filter ${gtest_filter} ============"
+    echo "============ done dsn.zookeeper.tests ${test_case} with gtest_filter ${gtest_filter} ============"
 done <gtest.filter
