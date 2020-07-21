90bd5869ad3ac5dee3787f655a7388f9cc835ab4
JiaShuo <js982986555@live.com>
2020-07-21 15:30:19 +0800

    update

--- include/dsn/tool/latency_tracer.h
+++ include/dsn/tool/latency_tracer.h
@@ -5,6 +5,9 @@
 #pragma once
 #include <dsn/service_api_c.h>
 #include <dsn/dist/fmt_logging.h>
+#include <dsn/tool-api/zlocks.h>
+#include <dsn/tool-api/task.h>
+#include <dsn/perf_counter/perf_counter_wrapper.h>
 
 namespace dsn {
 namespace tool {
@@ -12,7 +15,7 @@ namespace tool {
 /**
  * latency_tracer is a simple tool for tracking request time consuming in different stages, which
  * can help user find the latency bottleneck. user needs to use it to "add_point" in one stage,
- * which will record the name of point and the time_used. when the request is finshed, you can dump
+ * which will record the name of point and the timestamp. when the request is finshed, you can dump
  * the formated result.
  *
  * for example: one request experiences four stages, latency_tracer need be held by request and
@@ -41,76 +44,109 @@ namespace tool {
  * the "points" will record the all points' time_used_from_previous and time_used_from_start
 **/
 
-struct trace_point
-{
-    std::string name;
-    uint64_t ts;
-
-    trace_point(){};
-
-    trace_point(const std::string &name, uint64_t ts) : name(name), ts(ts){};
-};
 struct latency_tracer
 {
 
 public:
-    uint64_t id;
-    std::string type;
-    std::string start_name;
-    uint64_t start_time;
-    std::vector<trace_point> trace_points;
-    // sub_tracer can record such case:
-    // a_request-->start-->stageA -
-    //                            |-->stageB-->end
-    // b_request-->start-->stageA -
-    // a_request and b_request will share same sub_tracer from stageB
-    std::shared_ptr<latency_tracer> sub_tracer;
+    dsn::zrwlock_nr lock;
+
+    uint64_t id = 0;
+    std::string type = "mutation";
+    bool disable_trace = false;
+    std::map<int64_t, std::string> points;
+    // link_tracers is used for tracking the request which may transfer the other type,
+    // for example: rdsn "request" type will be transered "mutation" type, the "tracking
+    // responsibility" is also transfered the "mutation":
+    //
+    // stageA[request]--stageB[request]--
+    //                                  |-->stageC[mutation]
+    // stageA[request]--stageB[request]--
+    //
+    // from stageB, the tracer of request can link the tracer of mutation and continue tracking
+    // through mutation object
+    std::vector<std::shared_ptr<latency_tracer>> link_tracers;
 
 public:
-    latency_tracer(int id, std::string &start_name, const std::string &type)
-        : id(id), type(type), start_name(start_name), start_time(dsn_now_ns()){};
+    latency_tracer(int id, const std::string &start_name, const std::string &type)
+        : id(id), type(type)
+    {
+        points[dsn_now_ns()] = start_name;
+    };
 
-    // this method is called for any other method which will be recorded methed name and ts
+    // this method is called for any other method which will be recorded methed name and timestamp
     //
     // -name: generally, it is the name of that call this method. but you can define the more
     // significant name to show the events of one moment
     void add_point(const std::string &name)
     {
-        trace_points.emplace_back(trace_point(name, dsn_now_ns()));
+        if (disable_trace) {
+            return;
+        }
+
+        int64_t ts = dsn_now_ns();
+        dsn::zauto_write_lock write(lock);
+        points[ts] = name;
     }
 
-    void set_sub_tracer(std::shared_ptr<latency_tracer> tracer) { sub_tracer = tracer; }
-
-    std::string to_string() const
+    void add_link_tracer(std::shared_ptr<latency_tracer> &link_tracer)
     {
-        std::string trace;
-        uint64_t previous_time = start_time;
-        // todo(jiashuo) format more appropriately
-        for (const auto &point : trace_points) {
-            trace = fmt::format("{}\n\tTRACER[{}]:from_previous={}, from_start={}, ts={}, name={}",
-                                trace,
-                                id,
-                                point.ts - previous_time,
-                                point.ts - start_time,
-                                point.ts,
-                                point.name);
-            previous_time = point.ts;
+        if (disable_trace) {
+            return;
         }
 
-        if (sub_tracer == nullptr) {
-            return trace;
+        dsn::zauto_write_lock write(lock);
+        link_tracers.emplace_back(link_tracer);
+    }
+
+    void disable_trace() { disable_trace = true; }
+
+    //  threshold < 0: don't dump any trace points
+    //  threshold = 0: dump all trace points
+    //  threshold > 0: dump the trace point which time_used > threshold
+    void dump_trace_points(int threshold, std::string trace = std::string())
+    {
+        if (threshold < 0 || disable_trace) {
+            return;
         }
 
-        std::string trace_to_sub_trace =
-            fmt::format("\n\tTRACER[{}]:from_previous={}, from_start={}, ts={}, name={}",
-                        sub_tracer->id,
-                        sub_tracer->start_time - previous_time,
-                        sub_tracer->start_time - start_time,
-                        sub_tracer->start_time,
-                        sub_tracer->start_name);
+        if (points.empty()) {
+            return;
+        }
 
-        std::string sub_trace = sub_tracer->to_string();
-        return fmt::format("{}{}{}", trace, trace_to_sub_trace, sub_trace);
+        dsn::zauto_read_lock read(lock);
+
+        uint64_t start_time = points.begin()->first;
+        uint64_t time_used = points.rbegin()->first - start_time;
+
+        if (time_used < threshold) {
+            return;
+        }
+
+        uint64_t previous_time = points.begin()->first;
+        for (const auto &point : points) {
+            trace = fmt::format(
+                "{}\n\tTRACER[{:<10}]:name={:<50}, from_previous={:<20}, from_start={:<20}, "
+                "ts={:<20}, id={}",
+                trace,
+                type,
+                point.second,
+                point.first - previous_time,
+                point.first - start_time,
+                point.first,
+                id);
+            previous_time = point.first;
+        }
+
+        if (link_tracers.empty()) {
+            dwarn_f("TRACER:the trace as fallow\n{}", trace);
+        }
+
+        for (auto const &tracer : link_tracers) {
+            trace =
+                fmt::format("{}\n\tTRACE:The trace transfer the follow id[{}]", trace, tracer->id);
+            tracer->dump_trace_points(0, trace);
+        }
+        return;
     }
 };
 } // namespace tool
