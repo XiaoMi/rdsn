62b8fa824a88d8d0fe5102f4bfa16cbc5f9f8409
JiaShuo <js982986555@live.com>
2020-08-04 11:57:51 +0800

    fix review

--- src/utils/latency_tracer.h
+++ src/utils/latency_tracer.h
@@ -1,6 +1,19 @@
-// Copyright (c) 2017-present, Xiaomi, Inc.  All rights reserved.
-// This source code is licensed under the Apache License Version 2.0, which
-// can be found in the LICENSE file in the root directory of this source tree.
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
 #pragma once
 #include <dsn/utility/synchronize.h>
@@ -53,6 +66,13 @@ public:
     // -name: user specified name of the trace point
     void add_point(const std::string &stage_name);
 
+    // sub_tracer is used for tracking the request which may transfer the other type,
+    // for example: rdsn "rpc_message" will be convert to "mutation", the "tracking
+    // responsibility" is also passed on the "mutation":
+    //
+    // stageA[rpc_message]--stageB[rpc_message]--
+    //                                          |-->stageC[mutation]
+    // stageA[rpc_message]--stageB[rpc_message]--
     void set_sub_tracer(const std::shared_ptr<latency_tracer> &tracer);
 
     //  threshold < 0: don't dump any trace points
@@ -68,13 +88,6 @@ private:
     const std::string _name;
     const uint64_t _start_time;
     std::map<int64_t, std::string> _points;
-    // sub_tracer is used for tracking the request which may transfer the other type,
-    // for example: rdsn "rpc_message" will be convert to "mutation", the "tracking
-    // responsibility" is also passed on the "mutation":
-    //
-    // stageA[rpc_message]--stageB[rpc_message]--
-    //                                          |-->stageC[mutation]
-    // stageA[rpc_message]--stageB[rpc_message]--
     std::shared_ptr<latency_tracer> _sub_tracer;
 
     friend class latency_tracer_test;
