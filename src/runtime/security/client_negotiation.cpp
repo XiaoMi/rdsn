4f714c83f58009c9b1cadf2c8bef47a6500c514b
levy <zlw5307@163.com>
2020-08-05 14:54:25 +0800

    fix by review

--- src/runtime/security/client_negotiation.cpp
+++ src/runtime/security/client_negotiation.cpp
@@ -25,9 +25,7 @@ namespace security {
 
 client_negotiation::client_negotiation(rpc_session *session) : negotiation(session)
 {
-    _name = fmt::format("C_NEGO_L({})=>R({})",
-                        dsn_primary_address().to_string(),
-                        _session->remote_address().to_string());
+    _name = fmt::format("CLIENT_NEGOTIATION(SERVER={})", _session->remote_address().to_string());
 }
 
 void client_negotiation::start()
--- src/runtime/security/server_negotiation.cpp
+++ src/runtime/security/server_negotiation.cpp
@@ -24,9 +24,7 @@ namespace security {
 
 server_negotiation::server_negotiation(rpc_session *session) : negotiation(session)
 {
-    _name = fmt::format("S_NEGO_L({})=>R({})",
-                        dsn_primary_address().to_string(),
-                        _session->remote_address().to_string());
+    _name = fmt::format("SERVER_NEGOTIATION(CLIENT={})", _session->remote_address().to_string());
 }
 
 void server_negotiation::start()
