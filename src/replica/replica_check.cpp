d6aa0c7ddd4f767a365c04759d8adfefa650954a
Wu Tao <wutao1@xiaomi.com>
2020-11-02 14:24:46 +0800

    Update src/replica/replica_check.cpp

--- src/replica/replica_check.cpp
+++ src/replica/replica_check.cpp
@@ -176,7 +176,8 @@ void replica::on_group_check(const group_check_request &request,
         if (request.last_committed_decree > last_committed_decree()) {
             _prepare_list->commit(request.last_committed_decree, COMMIT_TO_DECREE_HARD);
         }
-        _split_mgr->secondary_parent_handle_split(request, response);
+        // the group check may trigger split on the secondary.
+        _split_mgr->trigger_secondary_parent_split(request, response);
         break;
     case partition_status::PS_POTENTIAL_SECONDARY:
         init_learn(request.config.learner_signature);
