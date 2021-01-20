e25cb8a04d8bd293159c60ff5291efecd6ac26e9
levy <zlw5307@163.com>
2021-01-20 16:48:56 +0800

    fix

--- src/replica/test/replica_test_base.h
+++ src/replica/test/replica_test_base.h
@@ -68,7 +68,9 @@ struct replica_test_base : replica_stub_test_base
         mu->data.updates.emplace_back(mutation_update());
         mu->data.updates.back().code =
             RPC_COLD_BACKUP; // whatever code it is, but never be WRITE_EMPTY
-        mu->data.updates.back().data.assign(data.data(), 0, data.length());
+        std::shared_ptr<char> ptr(dsn::utils::make_shared_array<char>(data.length()));
+        memcpy(ptr.get(), data.data(), data.length());
+        mu->data.updates.back().data.assign(ptr, 0, data.length());
         mu->client_requests.push_back(nullptr);
 
         // replica_duplicator always loads from hard disk,
