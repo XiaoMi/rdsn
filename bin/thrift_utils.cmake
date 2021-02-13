b8928a2a0a22ff76b6141990ce58efa1fa97793f
neverchanje <wutao.as.neverchanje@gmail.com>
2021-02-14 00:08:27 +0800

    refactor: remove generated thrift srcs from repo

---
+++ bin/thrift_utils.cmake
@@ -0,0 +1,69 @@
+##############################################################################
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+##############################################################################
+
+# Path of Thrift compiler has been determined from the outside thirdparties management.
+# Here we assume the path is found.
+find_program(THRIFT_COMPILER
+    NAMES
+        thrift
+    PATH_SUFFIXES
+        bin
+)
+
+set(THRIFT_GENERATED_FILE_PATH ${CMAKE_BINARY_DIR}/thrift-gen)
+if(NOT EXISTS ${THRIFT_GENERATED_FILE_PATH})
+    file(MAKE_DIRECTORY ${THRIFT_GENERATED_FILE_PATH})
+endif()
+include_directories(${THRIFT_GENERATED_FILE_PATH})
+
+# THRIFT_GENERATE_CPP is used to generate sources using the thrift compiler.
+#
+# Example:
+#
+# include(thrift_utils)
+# thrift_generate_cpp(RPC_META_THRIFT_SRCS RPC_META_THRIFT_HDRS rpc_meta.thrift)
+# add_library(
+#     dsn_rpc
+#     ${RPC_META_THRIFT_SRCS}
+#     ...
+# )
+function(THRIFT_GENERATE_CPP SRCS HDRS thrift_file)
+    if(NOT EXISTS ${thrift_file})
+        message(FATAL_ERROR "thrift file ${thrift_file} does not exist")
+    endif()
+
+    message(STATUS "THRIFT_GENERATE_CPP: ${thrift_file}")
+
+    exec_program(${THRIFT_COMPILER}
+        ARGS --out "${THRIFT_GENERATED_FILE_PATH}" --gen cpp ${thrift_file}
+        OUTPUT_VARIABLE __thrift_OUT
+        RETURN_VALUE THRIFT_RETURN)
+    if(NOT ${THRIFT_RETURN} EQUAL "0")
+        message(FATAL_ERROR "thrift-compiler exits with " ${THRIFT_RETURN} ": " ${__thrift_OUT})
+    endif()
+
+    set(${SRCS})
+    set(${HDRS})
+    file(GLOB_RECURSE __result_src "${CMAKE_BINARY_DIR}/${_target_dir}/*.cpp")
+    file(GLOB_RECURSE __result_hdr "${CMAKE_BINARY_DIR}/${_target_dir}/*.h")
+    list(APPEND ${SRCS} ${__result_src})
+    list(APPEND ${HDRS} ${__result_hdr})
+    set(${SRCS} ${${SRCS}} PARENT_SCOPE)
+    set(${HDRS} ${${HDRS}} PARENT_SCOPE)
+endfunction()
