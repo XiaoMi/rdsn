68270c72a0a52bd690ff184afa85caadae2ddf69
neverchanje <wutao.as.neverchanje@gmail.com>
2021-02-23 13:44:27 +0800

    fix compilation

--- include/dsn/dist/replication/replication_types.h
+++ include/dsn/dist/replication/replication_types.h
@@ -22,7 +22,7 @@
 #include "meta_admin_types.h"
 #include "partition_split_types.h"
 #include "duplication_types.h"
-#include "bulkload_types.h"
+#include "bulk_load_types.h"
 #include "backup_types.h"
 #include "consensus_types.h"
 #include "replica_admin_types.h"
