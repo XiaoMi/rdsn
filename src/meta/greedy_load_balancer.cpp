352ab657740b7bd4229966352a0c2b33623c2d33
heyuchen <heyuchen@xiaomi.com>
2021-03-02 11:45:05 +0800

    small refactor

--- src/meta/greedy_load_balancer.cpp
+++ src/meta/greedy_load_balancer.cpp
@@ -833,13 +833,8 @@ void greedy_load_balancer::greedy_balancer(const bool balance_checker)
             ddebug_f("skip to do primary balance for the ignored app[{}]", app->get_logname());
             continue;
         }
-        if (app->status != app_status::AS_AVAILABLE || app->is_bulk_loading)
+        if (app->status != app_status::AS_AVAILABLE || app->is_bulk_loading || app->splitting())
             continue;
-
-        if (app->helpers->split_states.splitting_count > 0) {
-            ddebug_f("app({}) is executing partition split, skip it.", app->app_name);
-            continue;
-        }
 
         bool enough_information = primary_balancer_per_app(app);
         if (!enough_information) {
@@ -885,7 +880,7 @@ void greedy_load_balancer::greedy_balancer(const bool balance_checker)
             continue;
         }
 
-        if (app->status != app_status::AS_AVAILABLE || app->is_bulk_loading)
+        if (app->status != app_status::AS_AVAILABLE || app->is_bulk_loading || app->splitting())
             continue;
 
         bool enough_information = copy_secondary_per_app(app);
