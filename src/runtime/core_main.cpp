75116736265dd34062aa5fa8abb1e7d8c1e63ea4
neverchanje <wutao.as.neverchanje@gmail.com>
2021-04-06 14:26:25 +0800

    fix

--- src/runtime/core_main.cpp
+++ src/runtime/core_main.cpp
@@ -24,18 +24,6 @@
  * THE SOFTWARE.
  */
 
-/*
- * Description:
- *     service core initialization
- *
- * Revision history:
- *     July, 2015, @imzhenyu (Zhenyu Guo), first version
- *     xxxx-xx-xx, author, fix bug about xxx
- */
-
-#include <dsn/service_api_c.h>
-#include <dsn/utility/ports.h>
-
 #include <dsn/tool/simulator.h>
 #include <dsn/tool/nativerun.h>
 #include <dsn/toollet/tracer.h>
@@ -43,7 +31,6 @@
 #include <dsn/toollet/fault_injector.h>
 
 #include <dsn/tool/providers.common.h>
-#include <dsn/utility/singleton.h>
 
 void dsn_core_init()
 {
@@ -57,8 +44,3 @@ void dsn_core_init()
     dsn::tools::register_toollet<dsn::tools::profiler>("profiler");
     dsn::tools::register_toollet<dsn::tools::fault_injector>("fault_injector");
 }
-
-#if defined(__linux__)
-#define STR_I(var) #var
-#define STR(var) STR_I(var)
-#endif
