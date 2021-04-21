30252f2e7cf2019c4cb0e01d25703d4832401c76
zhangyifan27 <chinazhangyifan@163.com>
2021-04-21 20:42:53 +0800

    fix

--- src/client/replication_ddl_client.cpp
+++ src/client/replication_ddl_client.cpp
@@ -995,7 +995,6 @@ dsn::error_code replication_ddl_client::do_restore(const std::string &backup_pro
         dsn::unmarshall(resp_task->get_response(), resp);
         if (resp.err == ERR_OBJECT_NOT_FOUND) {
             std::cout << "restore app failed: couldn't find valid app metadata" << std::endl;
-            return ERR_OK;
         } else if (resp.err == ERR_OK) {
             std::cout << "\t"
                       << "new app_id = " << resp.appid << std::endl;
