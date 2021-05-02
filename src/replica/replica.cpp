b395a4c09549d7be4ba771e9a8fd6382cfc38d54
lidingshengHHU <1946204069@qq.com>
2021-05-02 23:49:15 +0800

    bugfix: fix replica::init_table_level_latency_counter metric name error

--- src/replica/replica.cpp
+++ src/replica/replica.cpp
@@ -530,7 +530,7 @@ void replica::init_table_level_latency_counters()
         if (get_storage_rpc_req_codes().find(task_code(code)) !=
             get_storage_rpc_req_codes().end()) {
             std::string counter_str =
-                fmt::format("table.level.{}.latency(ns)@{}", task_code(code), _app_info.app_name);
+                fmt::format("table.level.{}.latency(ns)@{}", task_code(code).to_string(), _app_info.app_name);
             _counters_table_level_latency[code] =
                 dsn::perf_counters::instance()
                     .get_app_counter("eon.replica",
