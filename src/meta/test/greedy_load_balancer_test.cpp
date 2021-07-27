f8e7f157a3f7f141b707a0f012d062a38c78ff48
levy <zlw5307@163.com>
2021-07-27 18:06:38 +0800

    fix

--- src/meta/test/greedy_load_balancer_test.cpp
+++ src/meta/test/greedy_load_balancer_test.cpp
@@ -70,7 +70,7 @@ TEST(greedy_load_balancer, node_migration_info)
 
 TEST(greedy_load_balancer, get_skew)
 {
-    std::map<rpc_address, int32_t> count_map = {
+    std::map<rpc_address, uint32_t> count_map = {
         {rpc_address(1, 10086), 1}, {rpc_address(2, 10086), 3}, {rpc_address(3, 10086), 5},
     };
 
