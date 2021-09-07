23ed82aa45fb6147250eef8c23903cd4ab36243e
levy <zlw5307@163.com>
2021-09-07 10:29:50 +0800

    fix

--- src/meta/greedy_load_balancer.cpp
+++ src/meta/greedy_load_balancer.cpp
@@ -1008,7 +1008,7 @@ void greedy_load_balancer::balance_cluster()
     }
 
     cluster_replica_balance(
-        t_global_view, cluster_balance_type::COPY_SECONDARY, *t_migration_result);
+        t_global_view, cluster_balance_type::COPY_PRIMARY, *t_migration_result);
 }
 
 bool greedy_load_balancer::cluster_replica_balance(const meta_view *global_view,
