164b21af8a6a33544f4ab9cc11a8657f9859b1a7
jiashuo <js982986555@live.com>
2021-10-09 14:54:29 +0800

    fix

--- src/replica/test/mutation_log_test.cpp
+++ src/replica/test/mutation_log_test.cpp
@@ -503,7 +503,7 @@ TEST_F(mutation_log_test, reset_from)
     std::vector<mutation_ptr> expected;
     { // writing logs
         mutation_log_ptr mlog =
-            new mutation_log_private(_log_dir, 4, _gpid, _replica.get(), 1024, 512, 10000);
+            new mutation_log_private(_log_dir, 4, get_gpid(), _replica.get(), 1024, 512, 10000);
 
         EXPECT_EQ(mlog->open(nullptr, nullptr), ERR_OK);
 
@@ -522,7 +522,7 @@ TEST_F(mutation_log_test, reset_from)
 
     // create another set of logs
     mutation_log_ptr mlog =
-        new mutation_log_private(_log_dir, 4, _gpid, _replica.get(), 1024, 512, 10000);
+        new mutation_log_private(_log_dir, 4, get_gpid(), _replica.get(), 1024, 512, 10000);
     EXPECT_EQ(mlog->open(nullptr, nullptr), ERR_OK);
     for (int i = 0; i < 1000; i++) {
         mutation_ptr mu = create_test_mutation(2000 + i, "hello!");
@@ -553,7 +553,7 @@ TEST_F(mutation_log_test, reset_from_while_writing)
     std::vector<mutation_ptr> expected;
     { // writing logs
         mutation_log_ptr mlog =
-            new mutation_log_private(_log_dir, 4, _gpid, _replica.get(), 1024, 512, 10000);
+            new mutation_log_private(_log_dir, 4, get_gpid(), _replica.get(), 1024, 512, 10000);
         EXPECT_EQ(mlog->open(nullptr, nullptr), ERR_OK);
 
         for (int i = 0; i < 10; i++) {
@@ -568,7 +568,7 @@ TEST_F(mutation_log_test, reset_from_while_writing)
 
     // create another set of logs
     mutation_log_ptr mlog =
-        new mutation_log_private(_log_dir, 4, _gpid, _replica.get(), 1024, 512, 10000);
+        new mutation_log_private(_log_dir, 4, get_gpid(), _replica.get(), 1024, 512, 10000);
     EXPECT_EQ(mlog->open(nullptr, nullptr), ERR_OK);
 
     // given with a large number of mutation to ensure
