bc86018e833605ceda29468387c8c0e2ea38e5cf
tang yanzhao <smityz@qq.com>
2021-10-22 18:09:34 +0800

    delete header

--- include/dsn/utils/token_bucket_throttling_controller.h
+++ include/dsn/utils/token_bucket_throttling_controller.h
@@ -14,10 +14,15 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
 #pragma once
 
-#include <dsn/utility/TokenBucket.h>
+#include <dsn/dist/block_service.h>
+
+namespace folly {
+template <typename Clock>
+class BasicDynamicTokenBucket;
+
+} // namespace folly
 
 namespace dsn {
 namespace utils {
