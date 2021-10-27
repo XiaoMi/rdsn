66eea7ac3e2fabf6ac3d29e17f52612cd2a8ba36
jiashuo <js982986555@live.com>
2021-10-27 16:41:33 +0800

    update formant

--- src/utils/latency_tracer.cpp
+++ src/utils/latency_tracer.cpp
@@ -169,7 +169,7 @@ void latency_tracer::add_sub_tracer(const std::shared_ptr<latency_tracer> &trace
 
 std::shared_ptr<latency_tracer> latency_tracer::sub_tracer(const std::string &name)
 {
-    utils::auto_read_lock write(_sub_lock);
+    utils::auto_read_lock read(_sub_lock);
     auto iter = _sub_tracers.find(name);
     if (iter != _sub_tracers.end()) {
         return iter->second;
