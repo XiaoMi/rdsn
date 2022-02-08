a080ac403f6e17363c9d48645bdaf36138bbb2fa
jiashuo <js982986555@live.com>
2022-02-08 19:03:49 +0800

    init

--- include/dsn/utility/fail_point.h
+++ include/dsn/utility/fail_point.h
@@ -50,14 +50,14 @@
 
 /// The only entry to define a fail point with `void` function: lambda  function must be
 /// return void type. When a fail point is defined, it's referenced via the name.
-#define FAIL_POINT_INJECT_VOID_F(name, lambda)                                                     \
+#define FAIL_POINT_INJECT_NOT_RETURN_F(name, lambda)                                               \
     do {                                                                                           \
         if (dsn_likely(!::dsn::fail::_S_FAIL_POINT_ENABLED))                                       \
             break;                                                                                 \
         auto __Func = lambda;                                                                      \
         auto __Res = ::dsn::fail::eval(name);                                                      \
-        if (__Res == nullptr) {                                                                    \
-            __Func();                                                                              \
+        if (__Res != nullptr) {                                                                    \
+            __Func(*__Res);                                                                        \
         }                                                                                          \
     } while (0)
 
