af74cfcb6eb62cb2373c84a00e66321c440eae72
jiashuo <js982986555@live.com>
2022-02-14 17:08:08 +0800

    init

--- src/replica/test/replica_test.cpp
+++ src/replica/test/replica_test.cpp
@@ -14,7 +14,6 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-#pragma once
 
 #include <dsn/dist/replication/replica_envs.h>
 #include <dsn/utility/defer.h>
